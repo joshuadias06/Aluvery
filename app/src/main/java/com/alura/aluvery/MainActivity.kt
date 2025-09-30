f8d58aa4970a1952d1ad12549b48475bc70e6914
joshuadias06 <joshuadiasbf@gmail.com>
2025-09-30 14:20:32 -0300

    brush - align - offset

--- app/src/main/java/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/com/alura/aluvery/MainActivity.kt
@@ -9,18 +9,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.alura.aluvery.ui.theme.AluveryTheme
+import com.alura.aluvery.ui.theme.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AluveryTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ProductItem(
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -40,21 +49,39 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ProductItem(modifier: Modifier){
-    Column(){
-        Box(modifier = Modifier
-            .height(50.dp)
-            .background(Color.Blue)
-            .width(50.dp)
+fun ProductItem(modifier: Modifier) {
+    Column(
+        Modifier
+            .height(250.dp)
+            .width(200.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .height(100.dp)
+                .background(brush = Brush.horizontalGradient(colors = listOf(
+                    Purple500, Teal200
+                )))
+                .fillMaxWidth()
         )
-            Image(
-                painter = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = null
-            )
-            Text(text = "Texto 1")
-            Text(text = "Texto 2")
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            Modifier
+                .size(100.dp)
+                .offset(y = (-50).dp)
+                .clip(shape = CircleShape)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(text = "Text 1")
+        Text(text = "Text 2")
 
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProductItemPreview(){
+    ProductItem(modifier = Modifier)
 }
 
 
--- app/src/main/java/com/alura/aluvery/ui/theme/Color.kt
+++ app/src/main/java/com/alura/aluvery/ui/theme/Color.kt
@@ -7,5 +7,11 @@ val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
+
+val Purple200 = Color(0xFFBB86FC)
+
+val Teal200 = Color(0xFF03DAC5)
+
+val Purple500 = Color(0xFF6200EE)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
