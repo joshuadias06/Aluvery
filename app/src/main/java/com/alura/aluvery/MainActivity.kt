20fb3b162d4c4f6bdb44b9ef1d48589c213fc008
joshuadias06 <joshuadiasbf@gmail.com>
2025-09-30 12:48:52 -0300

    Create ProductItem component

--- app/src/main/java/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/com/alura/aluvery/MainActivity.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.alura.aluvery.ui.theme.AluveryTheme
@@ -26,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    MyFirstComposable(
-                        name = "Composable",
+                    ProductItem(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -36,19 +38,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
+
 @Composable
-fun ColumnPreview(){
-    Column(Modifier
-        .background(color = Color.Blue)
-        .padding(10.dp)
-    ){
-        Text(text = "Composable 1")
-        Text(text = "Composable 2")
+fun ProductItem(modifier: Modifier){
+    Column(){
+        Box(modifier = Modifier
+            .height(50.dp)
+            .background(Color.Blue)
+            .width(50.dp)
+        )
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = null
+            )
+            Text(text = "Texto 1")
+            Text(text = "Texto 2")
+
     }
 }
 
-@Composable
-fun MyFirstComposable(name: String, modifier: Modifier){
-    Text(text = "My First $name")
-}
+
+
