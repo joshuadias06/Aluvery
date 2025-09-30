b301150bc9b42221a8fce35825f9050c50e89bb9
joshuadias06 <joshuadiasbf@gmail.com>
2025-09-30 12:25:25 -0300

    Modifier

--- app/src/main/java/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/com/alura/aluvery/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,7 +14,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alura.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,25 +39,10 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun ColumnPreview(){
-    Column(){
-        Text(text = "Composable 1")
-        Text(text = "Composable 2")
-    }
-}
-
-@Preview
-@Composable
-fun RowPreview(){
-    Row(){
-        Text(text = "Composable 1")
-        Text(text = "Composable 2")
-    }
-}
-
-@Preview
-@Composable
-fun BoxPreview(){
-    Box(){
+    Column(Modifier
+        .background(color = Color.Blue)
+        .padding(10.dp)
+    ){
         Text(text = "Composable 1")
         Text(text = "Composable 2")
     }
