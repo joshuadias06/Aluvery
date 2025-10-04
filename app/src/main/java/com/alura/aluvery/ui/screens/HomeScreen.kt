dec3ed31fe9180944191d80f7f39daa83e017de5
joshuadias06 <joshuadiasbf@gmail.com>
2025-10-04 10:55:42 -0300

    Added search field

--- app/src/main/java/com/alura/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/alura/aluvery/ui/screens/HomeScreen.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,27 +26,33 @@ import com.alura.aluvery.ui.theme.AluveryTheme
 fun HomeScreen(
     sections: Map<String, List<Product>>
 ) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
-    ) {
-        item {
-            Spacer(Modifier)
-        }
-        for (section in sections) {
-            val title = section.key
-            val products = section.value
+    Column {
+        var text by remember{mutableStateOf("")}
+        OutlinedTextField(value = text, onValueChange = { newValue ->
+            text = newValue
+        }, Modifier.fillMaxWidth())
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
             item {
-                ProductSection(
-                    title = title,
-                    products = products
-                )
+                Spacer(Modifier)
             }
-        }
-        item {
-            Spacer(Modifier)
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductSection(
+                        title = title,
+                        products = products
+                    )
+                }
+            }
+            item {
+                Spacer(Modifier)
+            }
         }
     }
 }
